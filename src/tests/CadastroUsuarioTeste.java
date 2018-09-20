29060bbde04e2d75901e3603c538650c5e397912
Lucas Souza <lucas.soaresouza@gmail.com>
2018-09-20 19:55:03 -0300

    [BARRA VERMELHA] Teste de cadastro de usuario

--- src/tests/CadastroUsuarioTeste.java
+++ src/tests/CadastroUsuarioTeste.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import app.Usuario;
+
 public class CadastroUsuarioTeste {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCadastroUsuario() {
+		boolean resposta;
+		Usuario user;
+		resposta = Usuario.obterUsuario("Jabulane", "555.777.888-11");
+		assertTrue(resposta);
 	}
 
 }
