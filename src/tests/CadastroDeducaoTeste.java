5f17f9280156d0ab685b3879d94d601a70f8bd3e
Lucas Souza <lucas.soaresouza@gmail.com>
2018-09-20 23:55:42 -0300

    [BARRA VERMELHA] Teste de deducao sobre PA + Previdencia

--- src/tests/CadastroDeducaoTeste.java
+++ src/tests/CadastroDeducaoTeste.java
@@ -48,5 +48,29 @@ public class CadastroDeducaoTeste {
 		
 		
 	}
+	
+	
+	@Test
+	public void testCadastroPrevidenciaMaisPA() {
+		boolean resposta;
+		
+		Usuario user = Usuario.obterUsuario("Fulano", "111.222.333-44");
+		assertNotNull(user);
+		
+		Deducao ded = Deducao.obterDeducao("PA", 2);
+		assertNotNull(ded);
+		resposta = user.addDeducao(ded);
+		assertTrue(resposta);
+		assertEquals(user.getNumeroDeducoes(),1);
+		assertEquals(user.getTotalDeducoes(),2,0.01);
+		
+		Deducao ded2 = Deducao.obterDeducao("previdencia", 555.55);
+		assertNotNull(ded2);
+		resposta = user.addDeducao(ded2);
+		assertTrue(resposta);
+		assertEquals(user.getNumeroDeducoes(),2);
+		assertEquals(user.getTotalDeducoes(),557.55,0.01);
+		
+	}
 
 }
